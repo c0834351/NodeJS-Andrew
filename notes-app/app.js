8f57ae9c7cab0cfb7bddd7cabec299c60d0503d2
c0834351 <c0834351@mylambton.ca>
2022-06-04 03:50:32 -0400

    exercise-2

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,7 @@
 const add = require("./utils.js");
+const msg = require("./notes.js");
 const sum = add(7,3);
-
-
 console.log(sum);
+
+const a = msg("get Notes");
+console.log(a);
---
+++ notes-app/notes.js
@@ -0,0 +1,6 @@
+//exercise-2
+
+const msg = function getNotes(a){
+    return a;}
+
+module.exports = msg;
