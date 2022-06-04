6c006e909e56ba50aee9d909a14228d71d1b80d8
c0834351 <c0834351@mylambton.ca>
2022-06-04 04:39:27 -0400

    chalk package for colors and some other changes

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,13 +1,17 @@
 
 const validator = require('validator');
-
+const chalk = require("chalk");
 const msg = require("./notes.js");
+
+
 const a = msg("get Notes");
 console.log(a);
 
 console.log(validator.isURL('https://www3schools.com'));
 
+const sus = chalk.bgRed.green.bold('success')
 
+console.log(sus);
 // const add = require("./utils.js");
 // const sum = add(7,3);
 // console.log(sum);
