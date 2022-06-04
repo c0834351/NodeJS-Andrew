905aaf987508e0f3f48d0554ee42032203a494e4
c0834351 <c0834351@mylambton.ca>
2022-06-04 04:14:53 -0400

    about npm and validator package

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,14 @@
-const add = require("./utils.js");
-const msg = require("./notes.js");
-const sum = add(7,3);
-console.log(sum);
 
+const validator = require('validator');
+
+const msg = require("./notes.js");
 const a = msg("get Notes");
 console.log(a);
+
+console.log(validator.isURL('https://www3schools.com'));
+
+
+// const add = require("./utils.js");
+// const sum = add(7,3);
+// console.log(sum);
+
