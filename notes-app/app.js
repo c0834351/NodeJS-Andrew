bf093e7cda074ee5e1208ecbbcfe8b3c49460308
c0834351 <c0834351@mylambton.ca>
2022-06-04 05:45:13 -0400

    nodemon installed- argv-yargs packahge installeed

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,17 +1,81 @@
 
 const validator = require('validator');
 const chalk = require("chalk");
+const yargs = require("yargs");
 const msg = require("./notes.js");
+const { string } = require('yargs');
+const { parse } = require('path');
+
+// const command = process.argv[2];
+//console.log(process.argv);
+// if(command === 'add'){
+//     console.log('adding notes!');
+// }
+// else if(command === "remove"){
+//     console.log("removing the notes!");
+// }
 
 
-const a = msg("get Notes");
-console.log(a);
+//customize yargs version
+yargs.version('1.1.0');
+//add,remove,read,list
+yargs.command({
+    command : 'add',
+    describe: 'add a new notes',
+    builder: {
+        title: {
+            describe: 'note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: "notes app body",
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv){
+        console.log("title: ", argv.title);
+        console.log("body: ",argv.body);
+    }
+});
 
-console.log(validator.isURL('https://www3schools.com'));
+yargs.command({
+    command : 'remove',
+    describe: 'remove a new notes',
+    handler: function(){
+        console.log("removing a new note!");
+    }
+});
+yargs.command({
+    command : 'list',
+    describe: 'list a notes',
+    handler: function(){
+        console.log("listing out all note!");
+    }
+});
+yargs.command({
+    command : 'read',
+    describe: 'read a notes',
+    handler: function(){
+        console.log("reading a note!");
+    }
+})
 
-const sus = chalk.bgRed.green.bold('success')
+yargs.parse();
+//console.log(yargs.argv);
 
-console.log(sus);
+
+
+
+
+// const a = msg("get Notes");
+// console.log(a);
+
+// console.log(validator.isURL('https://www3schools.com'));
+
+// const sus = chalk.bgRed.green.bold('success')
+// console.log(sus );
 // const add = require("./utils.js");
 // const sum = add(7,3);
 // console.log(sum);
