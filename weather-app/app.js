57eb29e0dfc74adef7f588da8f6d3e661ab45d43
c0834351 <c0834351@mylambton.ca>
2022-06-28 01:25:36 -0400

    section-32

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,9 +1,9 @@
 const request = require('request')
 
-const url = 'http://api.weatherstack.com/current?access_key=a89f3bbe7b17970ee24216fa0509f9a0&query=New%20York'
+const url = 'http://api.weatherstack.com/current?access_key=a89f3bbe7b17970ee24216fa0509f9a0&query=New%20York&units=f'
 
-request({url: url}, (error,response) => {
-
-    const data = JSON.parse(response.body)
-    console.log(data.current.temperature);
+request({url: url, json: true}, (error,response) => {
+    console.log("In order to print in fahrenheit f or celsius m or kelvin s we use some units end of our url");
+    console.log("It is currently : " +response.body.current.temperature + " out. But feels like : " +response.body.current.feelslike + " out");
+    console.log("curently it is: " + response.body.current.weather_descriptions[0]);
 })
