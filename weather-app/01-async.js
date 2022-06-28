a5d0ff83a17f838ff3ffdc397897431e8f79c875
c0834351 <c0834351@mylambton.ca>
2022-06-28 00:25:20 -0400

    section-29

---
+++ weather-app/01-async.js
@@ -0,0 +1,9 @@
+console.log('starting');
+setTimeout(()=>{
+    console.log("2 seconds waiting...");
+}, 2000)
+
+setTimeout(()=>{
+    console.log("zero seconds wating..");
+},0)
+console.log('stopping');
